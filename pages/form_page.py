6caaab2a64b92e0e72ee49522cb007316439f864
Lipatnikova <aerovaya@gmail.com>
2024-01-15 13:32:43 +0400

    feat: add Allure

--- pages/form_page.py
+++ pages/form_page.py
@@ -1,3 +1,4 @@
+import allure
 import os
 import random
 
@@ -7,88 +8,100 @@ from generator.generator import get_person, generated_subject, generated_file, g
 from selenium.webdriver import Keys
 
 
-class FormsPage(BasePage):
+class FormPage(BasePage):
     def fill_first_name(self):
         info = next(get_person())
         first_name = info.first_name
-        self.send_keys_in_field(Locator.FIRST_NAME, first_name)
+        with allure.step("Заполнить поле First Name произвольной строкой"):
+            self.send_keys_in_field(Locator.FIRST_NAME, first_name)
         return first_name
 
     def fill_last_name(self):
         info = next(get_person())
         last_name = info.last_name
-        self.send_keys_in_field(Locator.LAST_NAME, last_name)
+        with allure.step("Заполнить поле Last Name произвольной строкой"):
+            self.send_keys_in_field(Locator.LAST_NAME, last_name)
         return last_name
 
     def fill_email(self):
         info = next(get_person())
         email = info.email
-        self.send_keys_in_field(Locator.EMAIL, email)
+        with allure.step("Заполнить поле Email строкой формата name@example.com"):
+            self.send_keys_in_field(Locator.EMAIL, email)
         return email
 
     def choose_gender(self):
         locator = Locator.GENDER
-        self.click_button(locator)               # gender click
+        with allure.step("Выбрать любое значение в Gender"):
+            self.click_button(locator)               # gender click
         gender_text = self.get_text(locator)
         return gender_text
 
     def fill_mobile_number(self):
         info = next(get_person())
         mobile = info.mobile
-        self.send_keys_in_field(Locator.PHONE_NUMBER, mobile)
+        with allure.step("Заполнить поле Mobile произвольными 10 цифрами"):
+            self.send_keys_in_field(Locator.PHONE_NUMBER, mobile)
         return mobile[:10]
 
     def choose_date_of_birth(self):
-        self.click_element(Locator.BIRTH_DAY)    # choose calender
-        # Select year:
-        self.click_button(Locator.YEARS_CHANGE)
-        year = self.get_text(Locator.YEARS)
-        self.click_element(Locator.YEARS)        # choose years
-        # Select month:
-        self.click_element(Locator.MONTHS_CHANGE)
-        month = self.get_text(Locator.MONTHS)
-        self.click_element(Locator.MONTHS)        # choose month
-        # Select day:
-        day = self.click_random_element(Locator.DAY_DATE_PICKER)
+        with allure.step("Заполнить поле Date of birth произвольной датой"):
+            self.click_element(Locator.BIRTH_DAY)    # choose calender
+            # Select year:
+            self.click_button(Locator.YEARS_CHANGE)
+            year = self.get_text(Locator.YEARS)
+            self.click_element(Locator.YEARS)        # choose years
+            # Select month:
+            self.click_element(Locator.MONTHS_CHANGE)
+            month = self.get_text(Locator.MONTHS)
+            self.click_element(Locator.MONTHS)        # choose month
+            # Select day:
+            day = self.click_random_element(Locator.DAY_DATE_PICKER)
         return day, month, year
 
     def fill_subjects(self):
-        subject_list = generated_subject()
-        for item in subject_list:
-            self.send_keys_in_field(Locator.SUBJECT, item)
-            self.send_keys_in_field(Locator.SUBJECT, Keys.RETURN)
+        with allure.step("Заполнить поле Subjects произвольной строкой"):
+            subject_list = generated_subject()
+            for item in subject_list:
+                self.send_keys_in_field(Locator.SUBJECT, item)
+                self.send_keys_in_field(Locator.SUBJECT, Keys.RETURN)
         return subject_list
 
     def choose_hobby(self):
         locator = Locator.HOBBIES
-        self.click_button(locator)                # choose hobby
+        with allure.step("Выбрать любое значение Hobbies"):
+            self.click_button(locator)                # choose hobby
         return self.get_text(locator)
 
     def select_picture(self):
-        file_name, path = generated_file()
-        self.send_keys_in_field(Locator.FILE_INPUT, path)
-        os.remove(path)
+        with allure.step("Загрузить любое изображение в поле Picture"):
+            file_name, path = generated_file()
+            self.send_keys_in_field(Locator.FILE_INPUT, path)
+            os.remove(path)
         return file_name.split('\\')[-1]
 
     def fill_current_address(self):
         info = next(get_person())
         current_address = info.current_address
-        self.send_keys_in_field(Locator.CURRENT_ADDRESS, current_address)
+        with allure.step("Заполнить поле Current Address произвольной строкой"):
+            self.send_keys_in_field(Locator.CURRENT_ADDRESS, current_address)
         return current_address
 
     def select_state_and_city(self):
-        state, city = generated_city()
-        new_city = city[random.randint(0, len(city) - 1)]
-        self.click_element(Locator.SELECT_STATE)
-        self.send_keys_in_field(Locator.STATE_INPUT, state)
-        self.send_keys_in_field(Locator.STATE_INPUT, Keys.RETURN)
-        self.click_element(Locator.SELECT_CITY)
-        self.send_keys_in_field(Locator.CITY_INPUT, new_city)
-        self.send_keys_in_field(Locator.CITY_INPUT, Keys.RETURN)
+        with allure.step("Выбрать любое значение в Select State и Select City"):
+            state, city = generated_city()
+            new_city = city[random.randint(0, len(city) - 1)]
+            self.click_element(Locator.SELECT_STATE)
+            self.send_keys_in_field(Locator.STATE_INPUT, state)
+            self.send_keys_in_field(Locator.STATE_INPUT, Keys.RETURN)
+            self.click_element(Locator.SELECT_CITY)
+            self.send_keys_in_field(Locator.CITY_INPUT, new_city)
+            self.send_keys_in_field(Locator.CITY_INPUT, Keys.RETURN)
         return state, new_city
 
     def click_submit(self):
-        self.element_is_visible(Locator.SUBMIT).send_keys(Keys.RETURN)
+        with allure.step("Нажать кнопку Submit"):
+            self.element_is_visible(Locator.SUBMIT).send_keys(Keys.RETURN)
 
     @property
     def get_modal_title(self):
--- tests/test_forms_page.py
+++ tests/test_forms_page.py
@@ -1,11 +1,17 @@
-from pages.form_page import FormsPage
+import allure
+from pages.form_page import FormPage
 from data.data_urls import DataUrls
 from data.data import ModalData
 
 
-class TestFormsPage:
+class TestFormPage:
+    @allure.title("Проверка заголовка всплывающего окна")
+    @allure.description("Проверка заголовка 'Thanks for submitting the form' после "
+                        "заполнения Формы регистрации корректными данными")
+    @allure.severity(allure.severity_level.MINOR)
+    @allure.label("owner", "Липатникова А.В.")
     def test_verify_modal_title(self, driver):
-        form_page = FormsPage(driver, DataUrls.FORM_URL)
+        form_page = FormPage(driver, DataUrls.FORM_URL)
         form_page.open()
         first_name = form_page.fill_first_name()
         last_name = form_page.fill_last_name()
@@ -19,5 +25,6 @@ class TestFormsPage:
         address = form_page.fill_current_address()
         state, city = form_page.select_state_and_city()
         form_page.click_submit()
-        assert form_page.get_modal_title == ModalData.EXPECTED_TITLE, \
-            "Modal title is not displayed correctly or contains unexpected text"
+        with allure.step("Проверить всплывающее окно с заголовком Thanks for submitting the form"):
+            assert form_page.get_modal_title == ModalData.EXPECTED_TITLE, \
+                "Modal title is not displayed correctly or contains unexpected text"
